a3c51c97304e0caf098f8a22b178cfb4194a666d
sangwookShin <shinsw6455@naver.com>
2021-02-21 21:59:58 +0900

    lombok 및 테스트코드 작성

---
+++ src/main/java/FirstProject/web/dto/HelloResponseDto.java
@@ -0,0 +1,13 @@
+package FirstProject.web.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDto {
+    private final String name;
+    private final int amount;
+
+
+}
