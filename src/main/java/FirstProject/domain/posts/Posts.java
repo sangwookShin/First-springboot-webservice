28a420c100ce049f65c9c54f6729a6203d35309a
sangwookShin <shinsw6455@naver.com>
2021-02-22 16:16:17 +0900

    게시글 저장 불러오기 Test

---
+++ src/main/java/FirstProject/domain/posts/Posts.java
@@ -0,0 +1,42 @@
+package FirstProject.domain.posts;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Posts {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String author;
+
+    @Builder
+    public Posts(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+}
+
+/*
+* @Entity 테이블과 링크될 클래스, 기본값으로 클래스의 카멜케이스 이름을 언더스코어 네이밍으로 테이블 이름을 매칭한다.
+* @Id 해달 테이블의 PK필드를 나타낸다.
+* @GeneratedValue PK의 생성규칙
+* @Column 테이블의 칼럼을 나타내며 굳이 선언하지 않더라도 해당 클래스의 필드는 모두 칼럼이 된다.
+* @NoArgsConstructor 기본생성자 자동 추가
+* @Getter 클래스 내 모든 필드의 Getter 메소드 자동생성
+* @Builder 해당 클래스의 빌더 패턴 클래스 생성, 생성자 상단에 선언 시 생성자에 포함된 필드만 빌더에 포함
+* */
---
+++ src/test/java/FirstProject/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,53 @@
+package FirstProject.domain.posts;
+
+import javafx.geometry.Pos;
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static FirstProject.domain.posts.Posts.builder;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @After
+    public void cleanup() {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void gaeshi_save_come() {
+        //given
+        String title = "테스트 게시글";
+        String content = "테스트 본문";
+
+        postsRepository.save(Posts.builder()
+                .title(title)
+                .content(content)
+                .author("shinsw6455@naver.com")
+                .build());
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+    }
+}
+/*
+* @After junit에서 단위테스트가 끝날때마다 수행되는 메소드를 지정, 보통 배포 전 전체 테스트를 수행할 때 테스트간 데이터 침범을 막기 위해 사용
+* postsRepository.save 테이블 posts에 insert/update 쿼리를 실행, id 값이 있다면 update, 없다면 insert 쿼리 실행
+* postsRepository.findAll 테이블 posts에 있는 모든 데이터를 조회
+* */
