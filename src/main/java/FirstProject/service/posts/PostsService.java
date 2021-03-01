2a343ada3f7645b69ae09f9dc54a5a69373bacc5
sangwookShin <shinsw6455@naver.com>
2021-03-02 00:24:40 +0900

    게시글 등록, 수정, 삭제 기능구현

--- src/main/java/FirstProject/service/posts/PostsService.java
+++ src/main/java/FirstProject/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package FirstProject.service.posts;
 
 import FirstProject.domain.posts.Posts;
 import FirstProject.domain.posts.PostsRepository;
+import FirstProject.web.dto.PostsListResponseDto;
 import FirstProject.web.dto.PostsResponseDto;
 import FirstProject.web.dto.PostsSaveRequestDto;
 import FirstProject.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -32,5 +36,18 @@ public class PostsService {
     public PostsResponseDto findById(Long id) {
         Posts entity = postsRepository.findById(id).orElseThrow(()-> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id).orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        postsRepository.delete(posts);
     }
 }
