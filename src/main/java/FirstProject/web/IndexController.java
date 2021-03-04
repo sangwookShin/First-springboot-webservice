cbfc2367fe19d33d303f560142878bbe5da3ae18
sangwookShin <shinsw6455@naver.com>
2021-03-04 23:05:15 +0900

    네이버 로그인 구현

--- src/main/java/FirstProject/web/IndexController.java
+++ src/main/java/FirstProject/web/IndexController.java
@@ -1,5 +1,6 @@
 package FirstProject.web;
 
+import FirstProject.config.auth.LoginUser;
 import FirstProject.config.auth.dto.SessionUser;
 import FirstProject.service.posts.PostsService;
 import FirstProject.web.dto.PostsResponseDto;
@@ -19,10 +20,8 @@ public class IndexController
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
 
         if(user != null) {
             model.addAttribute("userName", user.getName());
